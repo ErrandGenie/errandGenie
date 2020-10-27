fa55ab2888b40b077cfc4da4630e4e218747fc82
unknown <rashagibrill@gmail.com>
2020-10-27 10:30:29 +0000

    password toggle

--- sign.js
+++ sign.js
@@ -21,5 +21,18 @@ passwordToggle.addEventListener("click", function () {
     password.type = "password";
   }
   this.classList.toggle("fa-eye-slash");
-
 });
+
+var password = document.getElementById("password"),
+  confirm_password = document.getElementById("confirm_password");
+
+function validatePassword() {
+  if (password.value != confirm_password.value) {
+    confirm_password.setCustomValidity("Passwords Don't Match");
+  } else {
+    confirm_password.setCustomValidity("");
+  }
+}
+
+password.onchange = validatePassword;
+confirm_password.onkeyup = validatePassword;
