b747257a883a81e576c7c83ca7e47a76098d565c
unknown <rashagibrill@gmail.com>
2020-11-03 16:07:52 +0000

    latest

---
+++ js/pages.js
@@ -0,0 +1,12 @@
+const toggler = document.querySelector("div.dropdown-toggle");
+
+toggler.addEventListener("click", function () {
+  var menu = document.querySelector("div.menu");
+
+  if (menu.style.display === "none") {
+    menu.style.display = "block";
+  } else {
+    menu.style.display = "none";
+  }
+});
+
