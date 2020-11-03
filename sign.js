f4b0d2a960e88e6787d25ac6802581e247f5c2e0
unknown <rashagibrill@gmail.com>
2020-11-03 15:26:05 +0000

    backup

--- sign.js
+++ sign.js
@@ -1,15 +1,3 @@
-// const togglePassword = document.querySelector("#password-field");
-// const password = document.querySelector("#password");
-
-// togglePassword.addEventListener("click", function (e) {
-//   // toggle the type attribute
-//   const type =
-//     password.getAttribute("type") === "password" ? "text" : "password";
-//   password.setAttribute("type", type);
-//   // toggle the eye slash icon
-//   this.classList.toggle("fa-eye-slash");
-// });
-
 const passwordToggle = document.querySelector("#password-field");
 
 passwordToggle.addEventListener("click", function () {
