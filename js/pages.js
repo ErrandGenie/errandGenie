9fa71883914a0870080eaf97dcdc4cc86d08993b
unknown <rashagibrill@gmail.com>
2020-11-05 12:44:38 +0000

    errand genie

--- js/pages.js
+++ js/pages.js
@@ -1,15 +1,3 @@
-const toggler = document.querySelector("div.dropdown-toggle");
-
-toggler.addEventListener("click", function () {
-  var menu = document.querySelector("div.menu");
-
-  if (menu.style.display === "none") {
-    menu.style.display = "block";
-  } else {
-    menu.style.display = "none";
-  }
-});
-
 function myFunction(x) {
   x.classList.toggle("change");
 }
