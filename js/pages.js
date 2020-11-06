fd6cbc432c9c8e6eb0876d6620ce9524b7bd3347
unknown <rashagibrill@gmail.com>
2020-11-06 10:05:41 +0000

    errand genie

--- js/pages.js
+++ js/pages.js
@@ -8,5 +8,19 @@ function newInput() {
   input.classList.add("form-control");
   input.setAttribute("id", "inputAddress");
   input.setAttribute("placeholder", "Sakaman, Blue-Lagoon Street");
+
+  const icon = document.createElement("span");
+  icon.classList.add("fas", "fa-trash");
+  icon.style.color = "tomato";
+  icon.style.right = "20px";
+  icon.style.marginTop = "10px";
+  icon.style.position = "absolute";
+
+  icon.addEventListener("click", function () {
+    input.remove();
+    icon.remove();
+  });
+
+  address.append(icon);
   address.appendChild(input);
 }
