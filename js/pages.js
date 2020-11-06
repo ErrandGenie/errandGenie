4baa6505ee57addc28fadc7ac030309b33d5398a
unknown <rashagibrill@gmail.com>
2020-11-06 09:29:11 +0000

    errand genie

--- js/pages.js
+++ js/pages.js
@@ -1,3 +1,12 @@
 function myFunction(x) {
   x.classList.toggle("change");
 }
+
+function newInput() {
+  const address = document.querySelector("div.address");
+  const input = document.createElement("input");
+  input.classList.add("form-control");
+  input.setAttribute("id", "inputAddress");
+  input.setAttribute("placeholder", "Sakaman, Blue-Lagoon Street");
+  address.appendChild(input);
+}
